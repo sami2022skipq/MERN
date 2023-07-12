48797774730e0b988004f387909e6318dc8265dd
sami2022skipq <abdul.sami.skipq@gmail.com>
2023-07-12 16:39:34 +0500

    filters added

--- ecommerce-store/backend/utils/apiFeatures.js
+++ ecommerce-store/backend/utils/apiFeatures.js
@@ -1,3 +1,5 @@
+const { json } = require("express")
+
 class ApiFeatures {
     constructor(query, queryStr){
         this.query = query,
@@ -12,6 +14,29 @@ class ApiFeatures {
         }:{}
 
         this.query = this.query.find({...keyword})
+        return this
+    }
+
+    filter(){
+        const queryCopy = {...this.queryStr}
+
+        // Removing filed for category
+        const removeFields =["Keyword", "page", "limit"]
+        removeFields.forEach(key=> delete queryCopy[key])
+
+        // Filter for price and Rating
+
+        let queryStr = JSON.stringify(queryCopy)
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, key=> `$${key}`)
+
+        this.query = this.query.find(JSON.parse(queryStr))
+        return this
+    }
+    paggination(resultPerPage){
+        const currentPage = Number(this.queryStr.page) || 1
+
+        const skip = resultPerPage *(currentPage-1)
+        this.query = this.query.limit(resultPerPage).skip(skip)
         return this
     }
 }

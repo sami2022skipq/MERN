541465aacbdad8f513e9e277b95a8845a270c11c
sami2022skipq <abdul.sami.skipq@gmail.com>
2023-07-12 19:40:19 +0500

    update

--- ecommerce-store/backend/app.js
+++ ecommerce-store/backend/app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 
 const  product  = require('./routes/productsRoute')
+const user=  require('./routes/userRoute')
 const errorMiddleware =require('./middleware/error')
 
 app.use(express.json())
 
 app.use("/api/v1", product)
+app.use("/api/v1", user)
 
 // Error Middleware
 
